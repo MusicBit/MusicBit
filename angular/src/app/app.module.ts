fb526c0ab767a761a10a9e9a43a71c5ba499dec7
Tyree Galtney <zugzunited@gmail.com>
2022-09-27 18:39:21 -0500

    Fixed imports and validation on username/password. Backend still needed

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatCardModule,
     MatButtonModule,
--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   }
 
   loginForm : FormGroup = this.fb.group({
-    username : ['',[Validators.required, Validators.email]],
+    username : ['',[Validators.required, Validators.minLength(6)]],
     password : ['',[Validators.required, Validators.minLength(8)]]
   }
   )
