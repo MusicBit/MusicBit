006865a8b0422e210c3987dd25a3e44d07c589e8
Carson Smith <78703118+cdsccc@users.noreply.github.com>
2022-10-12 02:31:19 -0500

    Update RecAlgo.py
    Switched to using library Spotipy for Spotify API interactions.

--- src/RecAlgo.py
+++ src/RecAlgo.py
@@ -1,5 +1,10 @@
-import requests
-import json
+#pip install spotipy --upgrade
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import random
+from typing import NamedTuple
+import time
+
 
 # ----------------------------
 # SET BPM HERE
@@ -10,52 +15,106 @@ BPM = 150
 CLIENT_ID = ''
 CLIENT_SECRET = ''
 
-#-------------------------------------------------------
+#----------------------------------
 
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-# POST
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-    'scope': 'user-read-playback-state app-remote-control user-modify-playback-state user-read-currently-playing user-read-playback-position user-top-read user-read-recently-played',
-})
-# convert the response to JSON
-auth_response_data = auth_response.json()
+class Song(NamedTuple):
+    name: str
+    artist: str
+    id: str
+    bpm: int
+    uri: str
+#------------------------------------
+blackList=[]
+scope = 'user-top-read user-modify-playback-state user-read-playback-state'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri='http://127.0.0.1:9090',scope=scope))
 
-# save the access token
-access_token = auth_response_data['access_token']
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
+def getRecommendation():
+    if len(blackList)==0:
+        topSongs = sp.current_user_top_tracks(time_range='medium_term',limit=50)
+        randTopSong=topSongs['items'][random.randint(0,49)]
+        print("Chosen seed:",randTopSong['name'],'by',randTopSong['artists'][0]['name'])
+        seedID=str(randTopSong['id'])
+    else:
+        seedID=blackList[len(blackList)-1]
+    recommendations = sp.recommendations(seed_tracks={seedID},limit=20,target_tempo=BPM,min_tempo=BPM-10,max_temp=BPM+10)
+    recIDs=[]
+    for x in range(20):
+        recIDs.append(recommendations['tracks'][x]['id'])
 
-####THROWS ERROR 500#####
-#needs fix
-req1 = requests.get('https://api.spotify.com/v1/me/top/tracks',
-                    headers=headers,
-                    params={'limit': '1'})
-print(req1)
-#following code needs error 500 to  be fixed
-#topSong=req1.json()
-#topId = get id from json
+    songInfo=sp.audio_features(tracks=recIDs)
+    songList=[]
+    for k in range(20):
+        flag=True
+        curName=recommendations['tracks'][k]['name']
+        curArtist=recommendations['tracks'][k]['artists'][0]['name']
+        curID=recommendations['tracks'][k]['id']
+        curBPM=songInfo[k]['tempo']
+        curURI=recommendations['tracks'][k]['uri']
+        if len(blackList)==0:
+            songList.append(Song(curName,curArtist,curID,curBPM,curURI))
+        else:
+            for z in range(len(blackList)):
+                if curID==blackList[z]:
+                    flag=False
+            if flag:
+                songList.append(Song(curName,curArtist,curID,curBPM,curURI))
+    closestBPM=0
+    closestSong=0
+    for j in range(len(songList)):
+        if abs(songList[j].bpm-BPM)<abs(closestBPM-BPM):
+            closestBPM = songList[j].bpm
+            closestSong = j
+    print('Closest Song:',songList[closestSong].name,'by',songList[closestSong].artist)
+    print('    BPM',songList[closestSong].bpm)
+    blackList.append(songList[closestSong].id)
+    return songList[closestSong]
+
+
+def playSong(song,device):
+    dummyList=[]
+    dummyList.append(song.uri)
+    sp.start_playback(device_id=device,uris=dummyList)
+
+
+def getDevice():
+    result=sp.current_playback()
+    return result['device']['id']
+
+
+def nextCheck():
+    print('checking playback')
+    result=sp.current_playback()
+    return result['item']['duration_ms']-result['progress_ms']
 
 
 
+def qNextSong(song,device):
+    result=sp.add_to_queue(uri=song.uri,device_id=device)
 
-#change to use topId as seed rather than genres when error 500 is fixed
-# Request song list
-req2 = requests.get('https://api.spotify.com/v1/recommendations',
-                    headers=headers,
-                    params={'limit': '1', 'market': 'US', 'seed_genres': 'punk', 'target_tempo': BPM})
-songs = req2.json()
-print('Recommendation:')
-print(songs['tracks'][0]['name'], 'by', songs['tracks'][0]['artists'][0]['name'])
-songName = songs['tracks'][0]['name']
-songId = songs['tracks'][0]["id"]
-# Request Info about song (for BPM)
-req3 = requests.get('https://api.spotify.com/v1/audio-features/'+songId,
-                    headers=headers)
-songInfo = req3.json()
-songBPM=songInfo['tempo']
-print('    BPM:',songBPM)
 
+deviceID=getDevice()
+print('Device Found:',deviceID)
+songToPlay=getRecommendation()
+playSong(songToPlay,deviceID)
+flag=False
+curentLoop=1
+while True:
+    timeRemaining=nextCheck()
+    if timeRemaining<=30000 and flag==False:
+        try:
+            flag=True
+            BPM=random.randint(80,170) ##USED FOR TESTING REMOVE LATER
+            print('Random BPM:',BPM)
+            songToPlay=getRecommendation()
+            qNextSong(songToPlay,deviceID)
+            curentLoop=curentLoop+1
+            if curentLoop>20:
+                blackList.remove(blackList[0])
+                print('song removed from blacklist')
+        except:
+            print('Error Caught')
+    if timeRemaining>30000:
+        flag=False
+        time.sleep(25)
+    else:
+        time.sleep(25)
