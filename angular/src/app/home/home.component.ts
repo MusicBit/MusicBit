1f845ce19d9416323fe1db90bf85e375b3e7a41f
Tyree Galtney <zugzunited@gmail.com>
2022-10-24 15:49:33 -0500

    Added spotify login popup, need to clean up the home page some but popup works and redirects back to home page within the popup with access token in URL. Need to parse the token and store it to database

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -8,6 +8,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  clientID = "78151092cffd4fc18f99577b2a43cc65";
+  spotifyAuthEndpoint = "https://accounts.spotify.com/authorize";
+  redirectURL = "http://localhost:4200/home";
+  clientSecret = "bcd3a90646574005aef67e13fd575cf6";
+  scopes = "user-top-read";
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -18,4 +24,14 @@ export class HomeComponent implements OnInit {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC"; //clear login token
     this.router.navigate(['']);
   }
+
+  //got popup to open to connect spotify account, but dont know how to save token or close popup --Tyree
+  //https://leemartin.dev/creating-a-simple-spotify-authorization-popup-in-javascript-7202ce86a02f, following this guide but function calls is weird and just JS
+  onSpotifyLogin() {
+    let popup = window.open(`${this.spotifyAuthEndpoint}?client_id=${this.clientID}&redirect_uri=${this.redirectURL}&scope=${this.scopes}&response_type=token&show_dialog=true`,
+     'Login With Spotify', 'width=800,height=600')
+
+      
+  }
+
 }
