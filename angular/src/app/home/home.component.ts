7d0a268df8f2586cf4855ac65869fc125a0a6434
Xenotater <kgamer5000@gmail.com>
2022-10-25 20:50:56 -0500

    added methods to allow a spotify player device to be initialized

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+///  <reference types="@types/spotify-web-playback-sdk"/>
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -10,12 +11,21 @@ export class HomeComponent implements OnInit {
 
   clientID = "78151092cffd4fc18f99577b2a43cc65";
   spotifyAuthEndpoint = "https://accounts.spotify.com/authorize";
-  redirectURL = "http://localhost:4200/home";
-  scopes = "user-top-read";
+  redirectURL = window.location;
+  scopes = "streaming%20user-read-email%20user-read-private%20user-top-read";
+  token = "";
+  player!: Spotify.Player;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+    if (this.token)
+      window.opener.spotifyCallback(this.token);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      this.initPlayer();
+    };
   }
 
   onLogout() {
@@ -26,15 +36,60 @@ export class HomeComponent implements OnInit {
   //got popup to open to connect spotify account, but dont know how to save token or close popup --Tyree
   //https://leemartin.dev/creating-a-simple-spotify-authorization-popup-in-javascript-7202ce86a02f, following this guide but function calls is weird and just JS
   onSpotifyLogin() {
-      
-    window.location.href = `${this.spotifyAuthEndpoint}?client_id=${this.clientID}&redirect_uri=${this.redirectURL}&scope=${this.scopes}&response_type=token&show_dialog=true`;
-    let code = null;
-    let queryString = window.location.search;
-    if(queryString.length > 0) {
-        const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get("code");
-        console.log(code);
+    let popup = window.open(
+      `${this.spotifyAuthEndpoint}?client_id=${this.clientID}&response_type=token&redirect_uri=${this.redirectURL}&scope=${this.scopes}&show_dialog=true`,
+      'Login with Spotify',
+      'width=800, height=600, popup=yes, status=yes, toolbar=no, menubar=no, location=no,addressbar=no'
+    );
+
+    (window as any).spotifyCallback = (payload: any) => {
+      if (popup)
+          popup.close()
+      this.token = payload;
+      console.log(payload);
+      this.initPlayer();
     }
   }
 
+  initPlayer() {
+    if (this.token) {
+      this.player = new Spotify.Player({
+        name: 'MusicBit Player',
+        getOAuthToken: cb => { cb(this.token); },
+        volume: 0.5
+      });
+      console.log("Player" + this.player);
+
+      let player = this.player;
+      // Ready
+      player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+      });
+
+      // Not Ready
+      player.addListener('not_ready', ({ device_id }) => {
+          console.log('Device ID has gone offline', device_id);
+      });
+
+      player.addListener('initialization_error', ({ message }) => {
+          console.error(message);
+      });
+
+      player.addListener('authentication_error', ({ message }) => {
+          console.error(message);
+      });
+
+      player.addListener('account_error', ({ message }) => {
+          console.error(message);
+      });
+
+      player.connect();
+    }
+  }
+
+  playToggle() {
+    console.log("toggle");
+    this.player.togglePlay();
+  }
 }
+
