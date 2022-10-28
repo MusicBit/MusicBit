69df87a4d004c243fd707015a877c31a6aedec24
Tyree Galtney <zugzunited@gmail.com>
2022-10-27 21:51:24 -0500

    Updated register.php with new field for fitbit access token, updated register components to include form field for fitbit access token and link to the tutorial on how to get that for the user

--- angular/src/app/registration/registration.component.ts
+++ angular/src/app/registration/registration.component.ts
@@ -21,7 +21,8 @@ export class RegistrationComponent implements OnInit {
     firstName : ['',[Validators.required, Validators.maxLength(20)]],
     lastName : ['',[Validators.required, Validators.maxLength(20)]],
     username : ['',[Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
-    password : ['',[Validators.required, Validators.minLength(8)]]
+    password : ['',[Validators.required, Validators.minLength(8)]],
+    fitbitToken : ['', [Validators.required]]
   })
 
   onRegister() {
@@ -38,11 +39,15 @@ export class RegistrationComponent implements OnInit {
       responseType: "text" as const,
     };
 
-    this.http.post("https://www.musicbit.net/register.php", JSON.stringify({action: "register", fname: formData.firstName, lname: formData.lastName, user: formData.username, pass: formData.password}), httpOptions).subscribe(data => {
+    this.http.post("https://www.musicbit.net/register.php", JSON.stringify({action: "register", fname: formData.firstName, lname: formData.lastName, user: formData.username, pass: formData.password, fitbit_token: formData.fitbitToken}), httpOptions).subscribe(data => {
       console.log(data);
     });
 
     this.router.navigate(['']);
+  }
+
+  fitbitTutorial() {
+    window.open("https://drive.google.com/file/d/1obYDSvvzVhM9ufKRQ5EPXH7u0UR08J7O/view?usp=sharing", "_blank");
   }
 
 }
