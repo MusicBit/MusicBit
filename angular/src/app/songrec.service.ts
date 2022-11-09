e2af704165c1a4a7ab75c9250c12652317668b79
BDRoss <brossn7@gmail.com>
2022-11-09 01:48:33 -0600

    Finished function conversion
    Finished conversion of prototype python functions to Typescript functions in service. Might require some cleaning up after somebody with a useable Spotify token can test. A couple of globals might need to be shuffled around, and "top songs" api call is unique in it's call, and so might need additional fiddling.

--- angular/src/app/songrec.service.ts
+++ angular/src/app/songrec.service.ts
@@ -5,22 +5,35 @@ import SpotifyWebApi from 'spotify-web-api-js';
 import fetch from 'node-fetch'
 import * as qs from 'qs';
 
-// Might need to move this, according to codesandbox compiler
+// Currently some code/comments floating around that can be removed
+// after finalizing service
+
+// Might need to move this, according to codesandbox compiler,
+// but it makes sense here as globals?
 let spotify = new SpotifyWebApi();
-var blackList: string | any[] = [];
+var blackList: string[] = [];
 //var Spotify = require('spotify-web-api-js');
 //var s = new Spotify();
+
+interface Song{
+  Name: string;
+  Artist: string;
+  ID: string;
+  BPM: number;
+  URI: string;
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SongrecService {
   //let?
-  
 
-  
+
+
   constructor(private http: HttpClient) { }
-  
+
   async getTopTracks(token: string) {
     const httpOptions = {
       header: new HttpHeaders({
@@ -33,12 +46,14 @@ export class SongrecService {
     //    this.http.get("https://api.spotify.com/v1/me/top/tracks",qs.stringify({time_range: 'medium_term', limit: 50}), httpOptions).subscribe(data => {
     //wait tyree to save me
     //}
+
     //This might need to change, given usage of library
     let req = `{Authorization: \'Bearer \' ${token}, Content-Type: application/json, time_range: \'medium-term\', limit: 50}`
     return await spotify.getMyTopTracks(JSON.stringify(req), function (err, data) {
-      if (err) {console.error(err); return null}
+      if (err) { console.error(err); return null }
       else return data;
     });
+    spotify.getMyTopTracks({})
 
   }
   async getRecommendation(token: string, bpm: number) {
@@ -79,7 +94,7 @@ export class SongrecService {
       if (err) { console.error(err); return null; }
       else return data;
     });
-    let songList = []
+    let songList: Song[] = []
     for (let k = 0; k < 20; k++) {
       let flag = true;
       let curName = recommendations[k].name;
@@ -88,16 +103,66 @@ export class SongrecService {
       let curBPM = songInfo.audio_features[k].tempo;
       let curURI = recommendations[k].uri;
       if (blackList.length == 0) {
-        songList.push(/*Need to have a song struct?*/)
+        songList.push({ Name: curName, Artist: curArtist, ID: curID, BPM: curBPM, URI: curURI })
       }
       else {
         for (let z = 0; z < blackList.length; z++) {
           if (curID == blackList[z]) {
             flag = false;
           }
+          if (flag) {
+            songList.push({ Name: curName, Artist: curArtist, ID: curID, BPM: curBPM, URI: curURI });
+          }
         }
       }
     }
+    let closestBPM = 0;
+    let closestSong = 0;
+    for (let j = 0; j < songList.length; j++) {
+      if (Math.abs(songList[j].BPM - bpm) < Math.abs(closestBPM - bpm)) {
+        closestBPM = songList[j].BPM;
+        closestSong = j;
+      }
+    }
+    console.log('Closest song: ' + songList[closestSong] + " by " + songList[closestSong].Artist);
+    console.log("      BPM: " + songList[closestSong].BPM);
+    blackList.push(songList[closestSong].ID);
+    return songList[closestSong];
   }
+
+  async playSong(tarSong: Song, device: string) {
+    let dummyList = [];
+    dummyList.push(tarSong.URI);
+    // Hold response Promise as variable in case we want to do some checking
+    let response = await spotify.skipToNext({ device_id: device });
+  }
+
+  async getDevice() {
+    // Either spotify.getMyDevices(callback) or spotify.getMyCurrentPlaybackState(options, callback)
+    let result = (await spotify.getMyCurrentPlaybackState()).device.id;
+    // Actually may not have needed the callback at all - hope the async stuff doesn't break anything
+  }
+
+  async nextCheck() {
+    console.log('Checking playback');
+    let result = (await spotify.getMyCurrentPlaybackState());
+    if (result.progress_ms == null) {
+      return result.timestamp;
+    }
+    else if (result.item == null) {
+      return 0;
+    }
+    else {
+      return result.item.duration_ms - result.progress_ms;
+    }
+
+  }
+
+  async qNextSong(tarSong: Song, device: string) {
+    let result = await spotify.queue(tarSong.URI, { device_id: device });
+  }
+
+
+
 
 }
